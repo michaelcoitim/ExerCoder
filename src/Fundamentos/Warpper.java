735ca256178dd1ad60d271afb48b6e69ab9bc26c
michael coitim <michaelcoitim1@gmail.com>
2023-11-02 15:16:50 -0300

    Warppers

---
+++ src/Fundamentos/Warpper.java
@@ -0,0 +1,20 @@
+package Fundamentos;
+
+/**
+ *
+ * @author Michael Coitim
+ */
+public class Warpper {
+    public static void main(String[] args) {
+        Byte b =100;
+        Short s = 1000;
+        
+        //Integer.parseInt(entrada.next)) --> tranformando string em inteiros
+        Integer i = 10000;
+        Long l = 100000L;
+        
+        Float f =123.1f;
+        Double d =12354.123;
+        Boolean bo = Boolean.parseBoolean("true") ;
+    }
+}
