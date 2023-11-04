d1006e6af397d1bb73fedb787ac91caa5df937c7
michael coitim <michaelcoitim1@gmail.com>
2023-11-04 09:30:00 -0300

    conveções explícitas e implícita

--- src/Fundamentos/Warpper.java
+++ src/Fundamentos/Warpper.java
@@ -16,5 +16,16 @@ public class Warpper {
         Float f =123.1f;
         Double d =12354.123;
         Boolean bo = Boolean.parseBoolean("true") ;
+        Character c = '#';
+        
+        System.out.println(b.byteValue());
+        System.out.println(s.toString());
+        System.out.println(i * 3);
+        System.out.println(l / 3);
+        System.out.println(f);
+        System.out.println(d.shortValue());
+        System.out.println(bo.toString().toUpperCase());
+        System.out.println( c +" concatenando ");
+        System.out.println("");
     }
 }
