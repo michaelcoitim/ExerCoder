b442c9c57babe7eb4f151c053607d4887e9ef590
michael coitim <michael.coitim@datainfo.inf.br>
2025-03-14 17:14:15 -0300

    Logico

--- src/fundamentos/operadores/Logicos.java
+++ src/fundamentos/operadores/Logicos.java
@@ -7,4 +7,46 @@ package fundamentos.operadores;
  */
 public class Logicos {
     
+    public static void main(String[] args) {
+            boolean condicao1 = true;
+    boolean condicao2 = 2 >6 ;
+    
+    System.out.println(condicao1 && !condicao2);
+    System.out.println(condicao1 || condicao2);
+    System.out.println(condicao1 ^ condicao2);
+    System.out.println(!!condicao1);
+    System.out.println(!condicao2);
+    
+    //tabela verdade and/E &&
+    System.out.println("Tabla verdade E");
+    System.out.println(true && true);
+    System.out.println(true && false);
+    System.out.println(false && true);
+    System.out.println(false && false); //codigo morto 
+    
+    //tabela verdade or /OU ||
+    System.out.println("\nTabla verdade OU");
+    System.out.println(true || true);
+    System.out.println(true || false);
+    System.out.println(false || true);
+    System.out.println(false || false); 
+    
+     //tabela verdade xor / ou Exclusivo  ^
+    System.out.println("\nTabla verdade OU Exclusivo");
+    System.out.println(true  ^ true);
+    System.out.println(true  ^ false);
+    System.out.println(false  ^ true);
+    System.out.println(false  ^ false); 
+    
+     //tabela verdade xor / ou Exclusivo  ^
+    System.out.println("\nTabla verdade Not");
+    System.out.println(! true);
+    System.out.println(! false);
+   
+    
+    
+    }
+
+    
+  
 }
